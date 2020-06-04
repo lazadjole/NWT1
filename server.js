eac604a7333be2893cddd7de42806edffcbd60ba
lazadjole <62203309+lazadjole@users.noreply.github.com>
2020-06-04 22:54:32 +0200

    nesto

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
  
 const app = express();
 
-app.use(express.static(__dirname + '/dist/angularapp'));
+app.use(express.static(__dirname + '/dist/angularApp'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angularapp/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angularApp/index.html'));
 });
 
   app.listen(process.env.PORT || 8080);
